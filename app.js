f1609ccde1a4607204bc93e634e818367b887925
Rolando Perez <lindolo25@outlook.com>
2019-02-02 14:07:38 -0500

    made some necessary changes to integrate the REACT app.

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/users', usersRouter);
+app.use('*', indexRouter);
 
 module.exports = app;
