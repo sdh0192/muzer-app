06660c9f0e92b7005cee6691f75131b0ca7637b4
Scott Hastings <sdh0192@live.com>
2019-02-09 13:18:14 -0500

    adding html test routes for local auth

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/signin', function(req, res) {
+  res.sendFile(path.join(__dirname, "../test-html/signin.html"));
+});
+
+router.get('/signup', function(req, res) {
+  res.sendFile(path.join(__dirname, "../test-html/signup.html"));
+});
+
+
+router.get
 module.exports = router;
