4f47767e48fc04756976ffaa55dec3b7b17385ac
Rolando Perez <lindolo25@outlook.com>
2019-02-09 23:26:52 -0500

    refactored the auth routes, created an AuthController, added some security  features.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('*', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
 
 module.exports = router;
