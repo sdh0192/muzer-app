997a8e3954d3eb15346c4c461032727e7e0f90ab
Scott Hastings <sdh0192@live.com>
2019-02-10 21:53:08 -0500

    fixing google oauth function and adding cookie sessions

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieSession = require("cookie-session");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const passport = require("./validation/passport");
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require("./routes/authRoutes");
 
 var app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [process.env.COOKIE_KEY]
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
--- validation/passport.js
+++ validation/passport.js
@@ -6,75 +6,69 @@ const sha3_224 = require('js-sha3').sha3_224;
 var db = require("../models");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/webScraper";
 
+
 passport.use(
 	new GoogleStrategy(
-	{
-		clientID: process.env.GOOGLE_CLIENT_ID,
-		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		callbackURL: "/auth/google/callback"
-	},
-	(accessToken, refreshToken, profile, done) => 
-	{
-		console.log("access token: " + accessToken);
-		console.log("refresh token: " + refreshToken);
-		console.log(profile);
-		return done(null, profile);
-	})
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "/auth/google/callback"
+		},
+		(accessToken, refreshToken, profile, done) => {
+			db.GoogleAccount.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					// we already have a record with the given profile ID
+					done(null, existingUser);
+				} else {
+					// we don't have a record of a user with the given profile ID, make a new record
+					new GoogleAccount({ googleId: profile.id, email: profile.emails[0].value }).save().then(user => done(null, user));
+				}
+			});
+		})
 );
 
 passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-}, (email, password, cb) => 
-{
-	mongoose.connect(MONGODB_URI, {useNewUrlParser: true}, error => 
-    {
-        if(error) return cb(error);
+	usernameField: 'email',
+	passwordField: 'password'
+}, (email, password, cb) => {
+		mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, error => {
+			if (error) return cb(error);
 
-        db.Account.findOne({ email: email })
-			.populate('Profile')
-			.exec((error, user) => 
-			{
-				if(error)
-				{
-					mongoose.disconnect();
-					return cb(error);
-				}
-				else if (!user) 
-				{ 
-					mongoose.disconnect();
-					return cb(null, false);
-				}
-				else if (user.password != sha3_224(password))
-				{
-					mongoose.disconnect();
-					return cb(null, false);
-				}
-				else
-				{
-					mongoose.disconnect();
-					return cb(null, user);
-				}            
-			});
-	});
-}));
+			db.Account.findOne({ email: email })
+				.populate('Profile')
+				.exec((error, user) => {
+					if (error) {
+						mongoose.disconnect();
+						return cb(error);
+					}
+					else if (!user) {
+						mongoose.disconnect();
+						return cb(null, false);
+					}
+					else if (user.password != sha3_224(password)) {
+						mongoose.disconnect();
+						return cb(null, false);
+					}
+					else {
+						mongoose.disconnect();
+						return cb(null, user);
+					}
+				});
+		});
+	}));
 
 passport.serializeUser((user, cb) => cb(null, user.id));
 
-passport.deserializeUser(function(id, cb) 
-{
-    mongoose.connect(MONGODB_URI, {useNewUrlParser: true}, error => 
-    {
-        if(error) return cb(error);
+passport.deserializeUser(function (id, cb) {
+	mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, error => {
+		if (error) return cb(error);
 
-        db.AccountBase.findById(id)
+		db.AccountBase.findById(id)
 			.populate('Profile')
-			.exec(function (error, user)
-			{
-				if (error) 
-				{ 
+			.exec(function (error, user) {
+				if (error) {
 					mongoose.disconnect();
-					return cb(error); 
+					return cb(error);
 				}
 
 				mongoose.disconnect();
