0957cd990e394313b7105bf52b339e40e41291f6
Scott Hastings <sdh0192@live.com>
2019-03-01 19:48:09 -0500

    search controller additions and updates

---
+++ controllers/SearchController.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+var db = require("../models");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/webScraper";
+
+const SearchController = {
+    
+    getGenreSearch: function(req, res) {
+        //open db connection
+        mongoose.connect(MONGODB_URI, { useNewUrlParser: true}, error => {
+            if (error)
+                return res.json({ error: true, message: "Connection to the Database failed." });
+            //db query to pull profiles based on genre
+            db.Profiles.find()
+            //ADD SEARCH QUERIES FOR GENRE, INSTRUMENT, & NAME
+            .exec((error, profiles) => {
+                if (error) {
+                    mongoose.disconnect();
+                    return res.json({ error: true, message: "Connection to the Database failed." });
+                }
+                //if post found, return post
+                else if (profiles) {
+                    mongoose.disconnect();
+                    return res.json(profiles);
+                }
+            });
+        })
+    }
+}
+
+module.exports = SearchController;
